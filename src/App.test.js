5d507b358f48e9ab1094bf26c7cc22cf605b25d9
marc.palm@agualabs.com <m.palm@agualabs.de>
2018-11-06 16:23:59 +0100

    Adding github hooks

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Api } from './api';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  window.api = window.api || Api
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/api/github.js
@@ -0,0 +1,16 @@
+export const github = () => {
+  return {
+    auth: async (user, password) => {
+    },
+    getOrgs: async (user) => {
+    },
+    getRepos: async (org) => {
+    },
+    getBranches: async (org, repo) => {
+    },
+    getFeatures: async (org, repo, branch) => {
+    },
+    getContent: async (org, repo, branch, path, file) => {
+    } 
+  }
+}
